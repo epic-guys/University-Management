4b773b42a9ca66dd74f43adfb92c019cc19fe6ae
lucaboffo <lucaboffo02@gmail.com>
2023-05-10 13:23:52 +0200

    Create all table entities db

--- db/dump.sql
+++ db/dump.sql
@@ -23,3 +23,39 @@ CREATE TABLE Studenti (
                       ON UPDATE CASCADE
                       ON DELETE CASCADE
 );
+
+DROP TABLE IF EXISTS Docenti;
+CREATE TABLE Docenti (
+    cod_prof TEXT NOT NULL PRIMARY KEY,
+    cod_persona TEXT NOT NULL,
+    FOREIGN KEY (cod_persona) REFERENCES Persone (cod_persona)
+                     ON UPDATE CASCADE
+                     ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Esami;
+CREATE TABLE Esami (
+    cod_esame TEXT NOT NULL PRIMARY KEY,
+    nome_corso TEXT NOT NULL,
+    anno INT NOT NULL,
+    cfu INT NOT NULL
+);
+
+DROP TABLE IF EXISTS Prove;
+CREATE TABLE Prove (
+    cod_prova TEXT NOT NULL PRIMARY KEY,
+    scadenza DATE DEFAULT NULL,
+    cod_esame TEXT NOT NULL,
+    FOREIGN KEY (cod_esame) REFERENCES Esami(cod_esame)
+                   ON UPDATE CASCADE
+                   ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Appelli;
+CREATE TABLE Appelli(
+    data DATE NOT NULL PRIMARY KEY,
+    cod_prova TEXT NOT NULL,
+    FOREIGN KEY (cod_prova) REFERENCES Prove(cod_prova)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
+);
