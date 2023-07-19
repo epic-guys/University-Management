d2ff85c6deb43feb5fdf04cf18789aaf94433494
lucaboffo <lucaboffo02@gmail.com>
2023-07-19 23:33:33 +0200

    Create base.html and set up bootstrap template

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('dashboard.html')
+
 def main():
-    pass
+    app.run(debug=True)
 
 if __name__ == '__main__':
     main()
