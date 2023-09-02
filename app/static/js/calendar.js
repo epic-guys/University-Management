85a468e7dc55dca0982571229cd708e35e2f3822
lucaboffo <lucaboffo02@gmail.com>
2023-09-02 19:40:15 +0200

    Calendar con eventi (statici)
    Da installare tramite npm 'fullcalendar'

--- app/static/js/calendar.js
+++ app/static/js/calendar.js
@@ -1,9 +1,15 @@
-const Calendar = tui.Calendar;
+ document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+            events: [
+                {
+                    title: 'Evento',
+                    start: '2023-09-03',
+                    end: '2023-09-05'
+                }
 
-
-var Calendar = window.tui.Calendar;
-
-      var cal = new Calendar('#app', {
-        defaultView: 'month',
-        calendars: MOCK_CALENDARS,
+            ]
+        });
+        calendar.render();
       });
