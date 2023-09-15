be6a570096312f2dea22ea52cd3fe927287ef5c5
lucaboffo <lucaboffo02@gmail.com>
2023-09-15 16:07:30 +0200

    Aggiustamenti front-end

--- app/static/js/calendar.js
+++ app/static/js/calendar.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
           initialView: 'dayGridMonth',
+            editable: true,
             events: [
                 {
-                    title: 'Evento',
-                    start: '2023-09-03',
-                    end: '2023-09-05'
+                    id: 'e1',
+                    title: 'Esame di Basi di Dati',
+                    start: '2023-09-03'
                 }
 
             ]
